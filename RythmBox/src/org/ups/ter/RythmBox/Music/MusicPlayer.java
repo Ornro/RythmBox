7309f3439c9a839e67a8967e5193ed8835b0e9c1
alexburlacu11 <alexandru.burlacu11@gmail.com>
2014-02-14 17:27:41 +0100

    start menu

---
+++ RythmBox/src/org/ups/ter/RythmBox/Music/MusicPlayer.java
@@ -0,0 +1,27 @@
+package org.ups.ter.RythmBox.Music;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+
+public class MusicPlayer {
+	
+	public Sound song;
+
+	public MusicPlayer(String location) {
+		song = Gdx.audio.newSound(Gdx.files.internal(location));	
+	}
+	
+	public void playFrom(float f) {		
+		song.play(f);					
+	}
+	
+	public void playFrom() {
+		song.play();
+	}
+	
+	public void stop() {
+		song.stop();
+	}
+	
+	
+}
