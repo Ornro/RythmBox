cfcde758f9f8142892f247b262d6547471c09965
Xavier Varlez <xvarlez@gmail.com>
2014-02-14 09:39:38 +0100

    Displaying basic numbers

--- RythmBox-android/src/org/ups/ter/RythmBox/MainActivity.java
+++ RythmBox-android/src/org/ups/ter/RythmBox/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AndroidApplication {
         AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
         cfg.useGL20 = false;
         
-        initialize(new RythmBoxGDXGame(), cfg);
+        initialize(new NumbersDisplayer(), cfg);
     }
 }
